889b3b94618d7f256aca75806f7b2bb13cfff032
IvanMusila <imusila714@gmail.com>
2025-06-03 12:58:34 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set to '/' for root domain or '/subpath/' if needed
   build: {
-    outDir: 'dist', // Ensure this matches Netlify's publish dir
-    assetsDir: 'assets', // Default (where JS/CSS are stored)
-  }
+    outDir: 'dist',
+    assetsInlineLimit: 4096,
+  },
+  preview: {
+    allowedHosts: ['devserver-main--group-travel-planner.netlify.app']
+  },
 });
