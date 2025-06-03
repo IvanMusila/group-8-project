ad53aaa10a5add59c12d402597c12a9fd139861d
IvanMusila <imusila714@gmail.com>
2025-06-03 16:40:07 +0300

    vite config back to default

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsInlineLimit: 4096,
-  },
-  preview: {
-    allowedHosts: ['devserver-main--group-travel-planner.netlify.app']
-  },
-  // server: {
-  //   headers: {
-  //     'Content-Type': 'application/javascript'
-  //   }
-  // },
-});
+  plugins: [react()]
+})
